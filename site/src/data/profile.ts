250a702b9730f8bee9ce48d8ffd96c15dca25287
Vincent Gao <gaobing1230@gmail.com>
2026-05-24 15:16:04 +0200

    site: enlarge About copy and add profile links

--- site/src/data/profile.ts
+++ site/src/data/profile.ts
@@ -3,6 +3,7 @@ export const profile = {
   name: 'Bing Gao',
   role: 'CFD Engineer & Developer',
   tagline: 'External aerodynamics, CFD methodology & simulation tooling',
+  email: 'gaobing1230@gmail.com',
   linkedin: 'https://www.linkedin.com/in/bing-gao/',
   github: 'https://github.com/gaoflow',
 };
